b45ce6402d10e01f09f67907031dcaf887fc44cc
Gabriel Viana <gabrielfs.viana2@gmail.com>
2022-02-19 13:12:19 -0300

    att departament - name

--- src/main/java/com/project/professor/allocation/entity/Departament.java
+++ src/main/java/com/project/professor/allocation/entity/Departament.java
@@ -12,7 +12,7 @@ public class Departament {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(unique = true, nullable = false )
+	@Column(name = "name", unique = true, nullable = false )
 	private String name;
 
 	public Long getId() {
