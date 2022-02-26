5869e5727fd99d3543e6e05941c6dc6b4788dffc
Gabriel Viana <gabrielfs.viana2@gmail.com>
2022-02-26 16:05:09 -0300

    test professor repository

--- src/test/java/com/project/professor/allocation/repository/ProfessorRepositoryTest.java
+++ src/test/java/com/project/professor/allocation/repository/ProfessorRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.project.professor.allocation.repository;
 
 import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
+
+import com.project.professor.allocation.entity.Professor;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
@@ -23,10 +27,10 @@ public class ProfessorRepositoryTest {
 	    @Test
 	    public void findAll() {
 	        // Act
-	        
+	        List<Professor> professor = professorRepository.findAll();
 
 	        // Print
-	        
+	        System.out.println();
 	    }
 
 	    @Test
@@ -35,10 +39,11 @@ public class ProfessorRepositoryTest {
 	        
 
 	        // Act
-	        
+	        Optional<Professor> optional = professorRepository.findById(2L);
 
 	        // Print
-	        
+	        Professor prof = optional.orElse(null);
+	        System.out.println(prof);
 	    }
 
 	    @Test
@@ -68,25 +73,31 @@ public class ProfessorRepositoryTest {
 	    @Test
 	    public void save_create() throws ParseException {
 	        // Arrange
-	        
+	    	Professor professor = new Professor();
+			professor.setCpf("22222222222");
+			professor.setDepartmentId(1L);
+			professor.setName("Tiago Tiago");
 
 	        // Act
-	        
+			Professor prof = professorRepository.save(professor);
 
 	        // Print
-	        
+			System.out.println(prof);
 	    }
 
 	    @Test
 	    public void save_update() throws ParseException {
 	        // Arrange
-	        
+	    	Professor professor = new Professor();
+			professor.setDepartmentId(1L);
+			professor.setCpf("22222222223");
+			professor.setName("Gabriel Viana");
 
 	        // Act
-	        
+			Professor prof = professorRepository.save(professor);
 
 	        // Print
-	        
+			System.out.println(prof);
 	    }
 
 	    @Test
@@ -95,7 +106,7 @@ public class ProfessorRepositoryTest {
 	        
 
 	        // Act
-	        
+	    	professorRepository.deleteById(5L);
 	    }
 
 	    @Test
