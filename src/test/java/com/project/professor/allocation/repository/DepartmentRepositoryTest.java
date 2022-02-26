5dbd5e8087e1a0a764ab28260058f5d754526edf
Gabriel Viana <gabrielfs.viana2@gmail.com>
2022-02-26 15:43:27 -0300

    test department repository

--- src/test/java/com/project/professor/allocation/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/project/professor/allocation/repository/DepartmentRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.project.professor.allocation.repository;
 
 import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
+
+import com.project.professor.allocation.entity.Department;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
@@ -23,9 +27,9 @@ public class DepartmentRepositoryTest {
 	@Test
 	public void findAll() {
 		// Act
-
+		List<Department> department = departmentRepository.findAll();
 		// Print
-
+		System.out.println(department);
 	}
 
 	@Test
@@ -33,9 +37,10 @@ public class DepartmentRepositoryTest {
 		// Arrange
 
 		// Act
-
+		Optional<Department> optional = departmentRepository.findById(3L);
 		// Print
-
+		Department department = optional.orElse(null);
+		System.out.println(department);
 	}
 
 	@Test
@@ -61,21 +66,24 @@ public class DepartmentRepositoryTest {
 	@Test
 	public void save_create() throws ParseException {
 		// Arrange
-
+		Department department = new Department();
+		department.setName("Saúde");
 		// Act
-
+		Department depart = departmentRepository.save(department);
 		// Print
-
+		System.out.println(depart);
 	}
 
 	@Test
 	public void save_update() throws ParseException {
 		// Arrange
-
+		Department department = new Department();
+		department.setId(2L);
+		department.setName("Uniqualiti");
 		// Act
-
+		Department depart = departmentRepository.save(department);
 		// Print
-
+		System.out.println(depart);
 	}
 
 	@Test
@@ -83,7 +91,7 @@ public class DepartmentRepositoryTest {
 		// Arrange
 
 		// Act
-
+		departmentRepository.deleteById(2L);
 	}
 
 	@Test
